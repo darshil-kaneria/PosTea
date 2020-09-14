b408664f6ec134268ab7c7c8a087e7761a763acd
imemyself2 <dkaneria@purdue.edu>
2020-09-15 02:21:32 +0530

    check proper git structure

--- server/main.js
+++ server/main.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 23556
 app.get('/', (req, res)=>{
-    res.send('<p> TEST <p>');
+    res.send('<p> TEST second time <p>');
 });
 
 app.listen(PORT, ()=>console.log("listening on port "+PORT));
