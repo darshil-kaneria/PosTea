9f1b52fe703bf9ba2f86e6e4d2b2fb26e774ba8c
imemyself2 <dkaneria@purdue.edu>
2020-09-15 03:40:15 +0530

    Added comments for ease of understanding

--- server/main.js
+++ server/main.js
@@ -1,11 +1,23 @@
+/**
+ * Create a child process which executes test_concurrency_one.js
+ * The child process is created when a client hits the '/' endpoint.
+ * To test this functionality, go to postea-server.herokuapp.com/?number=<enter_number>
+ * To test different execution times, try numbers like 100000000, 1000000, 100 in place of <enter_number>
+ * Do not exceed more than 255 processes since that is how many processes heroku supplies with the free dyno that we are currently running.
+ */
+
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 23556
+
+//Create a fork object
 const fork = require("child_process").fork;
 app.get('/', (req, res)=>{
+    
     const child = fork('./test_concurrency_one.js');
     child.send({"number": parseInt(req.query.number)});
     child.on("message", message => res.send(message));
+
 });
 
 app.listen(PORT, ()=>console.log("listening on port "+PORT));
--- server/test_concurrency_one.js
+++ server/test_concurrency_one.js
@@ -1,16 +1,24 @@
+/**
+ * This file contains a test function that simply adds number upto what is specified in the query.
+ * This file is executed on a seperate process. The process can listen to it's parent process for any messages and reply back with data (in our case, with JSON) as needed.
+ * Reminder: Do not exceed more than 255 process on our heroku dyno.
+ */
+
 process.on("message", message => {
     var check_val = test_func(message.number);
-    process.send({"number": check_val});
-    process.exit();
+    process.send({"number": check_val}); // This is returned to the parent process
+    process.exit();  // It is very important to exit, or else heroku server will start accumulating orphaned processes.
 })
 
 function test_func(number){
 
-    console.log("process id: "+process.pid+", number: "+number);
-    var randomLol = 0;
+    console.log("process id: "+process.pid+", number: "+number); // Check the output on heroku logs
+    var countNumber = 0;
+
+    // simple loop that adds upto a certain number
     for(i = 0; i < number; i++){
-        randomLol++;
+        countNumber++;
     }
-    return randomLol;
+    return countNumber;
 
 }
