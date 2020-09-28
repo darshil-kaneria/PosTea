dfa41da57104080f9eb58c05af529389ed713efa
imemyself2 <dkaneria@purdue.edu>
2020-09-28 22:55:24 +0530

    Cleaned up code

--- server/main.js
+++ server/main.js
@@ -9,18 +9,6 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 23556;
-const password = process.env.DB_ACCESS;
-const mysql = require('mysql');
-
-// conn.getConnection(function(err, connection) {
-//     if (err) {
-//       return console.error('error: ' + err.message);
-//     }
-  
-//     console.log('Database connection established');
-//     connection.release();
-//   });
-
   
 //Create a fork object - test concurrency only
 const fork = require("child_process").fork;
@@ -45,64 +33,4 @@ handlePosts.on("message", message => res.send(message));
 
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 app.listen(PORT, ()=>console.log("listening on port "+PORT));
-
-// app.get('/profile', (req,res)=> {
-//   addUsername(req.username, req.is_private, req.name, req.biodata)
-// });
-
-// function addUserInfo(username, is_private, name, biodata) {
-// var sql = "INSERT INTO profile (username, is_private, name, bio_data) VALUES ?";
-//   var values = [
-//     [username, is_private, name, biodata]
-//     ];
-//   con.query(sql, [values], function (err, result) {
-//     if (err) throw err;
-//   });
-// }
-// app.get('/username', (req,res)=>{
-//   addUsername(req.username)
-// });
-
-// function addUsername(username) {
-//   var currdate = new Date();
-//   var sql = "INSERT INTO account (username, acc_creat_date) VALUES ("+username+","+currdate+")";
-//   conn.query(sql, function (err, result) {
-//     if (err) throw err;
-//     console.log("1 record inserted");
-// });}
