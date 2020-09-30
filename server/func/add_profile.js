107dcf348e212e9a2eb44c76617c0eaa3d8c715e
imemyself2 <dkaneria@purdue.edu>
2020-10-01 00:24:49 +0530

    Promise conflicts solved

--- server/func/add_profile.js
+++ server/func/add_profile.js
@@ -1,37 +1,46 @@
 const db = require('./db_connection.js');
 
 process.on("message", message => {
-    db.conn.getConnection(async function(err, connection) {
+    db.conn.getConnection(function(err, connection) {
           if (err) {
             return console.error('error: ' + err.message);
           }
           
           console.log('Database connection established');
-          await addUser(message.username, connection);
-          connection.release();
-          process.send({"userAdded": message.username});
-          process.exit(); // It is very important to exit, or else heroku server will start accumulating orphaned processes.
-          
+          addUser(message.username, connection).then(function(answer){
+            console.log(answer);
+            connection.release();
+            process.send({"userAdded": message.username});
+            process.exit();
+          }) 
         });
+        
 });
 
 
 
-const addUser = async function(user, connection) {
-
+ function addUser(user, connection) {
 
     var username = user;
+    var selectQuery = "SELECT * FROM account";
     var addProfileQuery = "INSERT INTO account (username, acc_creat_date) VALUES ?";
     var curr_date = new Date().toISOString().slice(0, 19).replace('T', ' ');
     var values = [[username, curr_date]];
-    await connection.query(addProfileQuery, [values], function (err, result) {
-          if (err) {
-            console.log(err);
-            throw err;}
-          console.log();
-          return result;
-        });
-      
+    return new Promise(function(resolve, reject) {
+       connection.query(selectQuery, function (err, result) {
+        if (err) {
+          console.log(err);
+          throw err;}
+        try {
+          resolve(result);
+        }
+        catch (error){
+          throw err;
+        }
+        return;
+      });
+    })
+    
   };
 
   
