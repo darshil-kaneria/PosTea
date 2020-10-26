a7cb354fe8fab90117ed52fcb4c6ba40aa8d1f19
imemyself2 <dkaneria@purdue.edu>
2020-10-27 03:44:45 +0530

    Refresh timeline resolved. BIG UPDATE

--- server/func/refreshTimeline.js
+++ server/func/refreshTimeline.js
@@ -21,91 +21,171 @@ process.on("message", message => {
 
 refreshTimeline = async (profileID, offset, time, connection) => {
 
+    var followingList = [];
+    var followingListString;
+    var getOffset;
+    var isTimeNull = false;
+   
+    console.log(time);
     //QUERY 1 - to select the dynamic offset
-    var getOffset = "SELECT COUNT(*) FROM user_post WHERE profile_id in (1,2) AND creation_date > '?' ORDER BY creation_date DESC";   
-
-    var getNumPosts = "SELECT profile_id, COUNT(*) FROM user_post WHERE profile_id = " + String(profileID);
-    var query = "SELECT * FROM user_post WHERE profile_id = " + String(profileID) + " ORDER BY creation_date DESC LIMIT " + String(offset) + ", 3"; // change if condition below if you change limit
+    
+    var getFollowing = `SELECT * FROM user_follower WHERE user_follower.profile_id = ${profileID}`;
+    
+    // var query = "SELECT * FROM user_post WHERE profile_id = " + String(profileID) + " ORDER BY creation_date DESC LIMIT " + String(offset) + ", 3"; // change if condition below if you change limit
     return new Promise(async (resolve, reject) => {
-        await connection.query(getNumPosts, async (err, result) => {
-            if (err) {
-                console.log(err);
+        await connection.query({sql: getFollowing, timeout: 7000}, async (err, result) => {
+
+            if(err){
                 reject(err.message);
             }
-            result = JSON.stringify(result);
-            // result = JSON.parse(result);
-            var numOccurances = result.substring(28, result.indexOf("}"));
-            if (offset >= numOccurances - 2) { // change if you change limit
-                var limit = numOccurances - offset;
-                var newquery = "SELECT * FROM user_post WHERE profile_id = " + String(profileID) + " ORDER BY creation_date DESC LIMIT " + String(offset) + ", " + String(limit); // change if condition below if you change limit
-                await connection.query({sql: newquery, timeout: 7000}, (err, result) => {
-                    if (err && err.code === 'PROTOCOL_SEQUENCE_TIMEOUT') {
-                        var dict = {
-                            "error": 2
-                        }
-                        process.send(dict);
-                        reject('Request timed out after 15 seconds');
-                      }
-                    if (err) {
-                        console.log("error: " + err.message);
-                        reject(err.message);
-                    }
-    
-                    result = JSON.stringify(result);
-                    result = JSON.parse(result);
-                    // var list = [];
-                    // for (i = 0; i < length(result); i++) {
-    
-                    // }
-                    var dict = {
-                        "result": result,
-                        "error": 1
-                    }
-                    process.send(dict);
-                    console.log("Query Complete");
-                    // connection.release();
-                    resolve(result);
-                });
-            } else {
-                await connection.query({sql: getOffset, timeout: 7000}, time, (err, result) => {
-                    if (err) {
-                        reject(err.message)
-                    }
-                    // result = JSON.stringify(result);
-                    // result = JSON.parse(result);
-                    console.log("result is .....");
-                    console.log(result);
-                    resolve(result);
-                });
-                await connection.query({sql: query, timeout: 7000}, (err, result) => {
-                    if (err && err.code === 'PROTOCOL_SEQUENCE_TIMEOUT') {
-                        var dict = {
-                            "error": 2
-                        }
-                        process.send(dict);
-                        throw new Error('Request timed out after 15 seconds');
-                      }
-                    if (err) {
-                        console.log("error: " + err.message);
-                        reject(err.message);
-                    }
-    
-                    result = JSON.stringify(result);
-                    result = JSON.parse(result);
-                    // var list = [];
-                    // for (i = 0; i < length(result); i++) {
-    
-                    // }
-                    var dict = {
-                        "result": result,
-                        "error": 0
-                    }
-                    process.send(dict);
-                    console.log("Query complete");
-                    // connection.release();
-                    resolve(result);
-                });
+            for(var i = 0; i < result.length; i++){
+                followingList.push(parseInt(result[i].follower_id));
             }
+            console.log(followingList);
+            followingListString = followingList.join(',');
+            followingListString = "("+followingListString+")";
+            console.log(followingListString);
+
+            if(time == null){
+                // getOffset = `SELECT COUNT(*) as offs FROM user_post WHERE profile_id in ${followingListString} ORDER BY creation_date DESC`;
+                isTimeNull = true;
+                
+            }
+
+            var getNumPosts = "SELECT profile_id, COUNT(*) FROM user_post WHERE profile_id in " +followingListString;
+                await connection.query(getNumPosts, async (err, result) => {
+                        if (err) {
+                            console.log(err);
+                            reject(err.message);
+                        }
+                        result = JSON.stringify(result);
+                        // result = JSON.parse(result);
+                        var numOccurances = result.substring(28, result.indexOf("}"));
+                        if(isTimeNull){
+
+                            var query = "SELECT * FROM user_post WHERE profile_id in "+followingListString+" ORDER BY creation_date DESC LIMIT " + String(offset) + ", 3"; // change if condition below if you change limit
+                            await connection.query({sql: query, timeout: 7000}, (err, result) => {
+                            if (err && err.code === 'PROTOCOL_SEQUENCE_TIMEOUT') {
+                                var dict = {
+                                    "error": 2
+                                }
+                                process.send(dict);
+                                throw new Error('Request timed out after 15 seconds');
+                              }
+                            if (err) {
+                                console.log("error: " + err.message);
+                                reject(err.message);
+                            }
+            
+                            result = JSON.stringify(result);
+                            result = JSON.parse(result);
+                            // var list = [];
+                            // for (i = 0; i < length(result); i++) {
+            
+                            // }
+                            var dict = {
+                                "result": result,
+                                "error": 0
+                            }
+                            process.send(dict);
+                            console.log("Query complete");
+                            // connection.release();
+                            resolve(result);
+                        });
+
+                        }
+                        var getOffset = `SELECT COUNT(*) as offs FROM user_post WHERE profile_id in ${followingListString} AND creation_date > ${time} ORDER BY creation_date DESC`;   
+                     
+                    // GET FOLLOWING LIST
+                    await connection.query({sql: getOffset, timeout: 7000}, [time], async (err, result) => {
+                        
+                        
+                            if (err) {
+                            reject(err.message)
+                        }
+                        // result = JSON.stringify(result);
+                        // result = JSON.parse(result);
+                        console.log("result is .....");
+                        console.log(result[0].offs);
+                        console.log(offset);
+                        offset = parseInt(result[0].offs) + parseInt(offset);
+                        console.log(offset);
+                         
+                        if (offset >= numOccurances - 2) { // change if you change limit
+                            var limit = numOccurances - offset;
+                            var newquery = "SELECT * FROM user_post WHERE profile_id in " +followingListString+ " ORDER BY creation_date DESC LIMIT " + String(offset) + ", " + String(limit); // change if condition below if you change limit
+                            await connection.query({sql: newquery, timeout: 7000}, (err, result) => {
+                                if (err && err.code === 'PROTOCOL_SEQUENCE_TIMEOUT') {
+                                    var dict = {
+                                        "error": 2
+                                    }
+                                    process.send(dict);
+                                    reject('Request timed out after 15 seconds');
+                                  }
+                                if (err) {
+                                    console.log("error: " + err.message);
+                                    reject(err.message);
+                                }
+                
+                                result = JSON.stringify(result);
+                                result = JSON.parse(result);
+                                // var list = [];
+                                // for (i = 0; i < length(result); i++) {
+                
+                                // }
+                                var dict = {
+                                    "result": result,
+                                    "error": 1
+                                }
+                                process.send(dict);
+                                console.log("Query Complete");
+                                // connection.release();
+                                resolve(result);
+                            });
+                        }
+                        else{
+
+                            var query = "SELECT * FROM user_post WHERE profile_id in "+followingListString+" ORDER BY creation_date DESC LIMIT " + String(offset) + ", 3"; // change if condition below if you change limit
+                        await connection.query({sql: query, timeout: 7000}, (err, result) => {
+                        if (err && err.code === 'PROTOCOL_SEQUENCE_TIMEOUT') {
+                            var dict = {
+                                "error": 2
+                            }
+                            process.send(dict);
+                            throw new Error('Request timed out after 15 seconds');
+                          }
+                        if (err) {
+                            console.log("error: " + err.message);
+                            reject(err.message);
+                        }
+        
+                        result = JSON.stringify(result);
+                        result = JSON.parse(result);
+                        // var list = [];
+                        // for (i = 0; i < length(result); i++) {
+        
+                        // }
+                        var dict = {
+                            "result": result,
+                            "error": 0
+                        }
+                        process.send(dict);
+                        console.log("Query complete");
+                        // connection.release();
+                        resolve(result);
+                    });
+
+                        }
+
+                        
+                        
+                    });
+                    
+                    
+                    // resolve(result);
+                });
+           
+            
             return result;
         });
     });
