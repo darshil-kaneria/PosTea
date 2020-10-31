2bd28acb5617a0bb6492de91cc5ae5f2be6369c0
shah444 <shah444@purdue.edu>
2020-10-31 21:04:41 +0530

    Added logic for retrieving trending posts

---
+++ front-end/postea_frontend/lib/data_models/process_trending.dart
@@ -0,0 +1,111 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'post.dart';
+
+class ProcessTrending {
+  int profile_id;
+
+  ProcessTrending({this.profile_id});
+
+  List<Post> postList = [];
+  var firstPostTime = null;
+  var offset = 0;
+  var temp;
+  bool isEnd = false;
+  bool postRetrieved = true;
+  Map<String, dynamic> posts;
+
+  Future<http.Response> getPosts() async {
+    http.Response resp;
+    if (isEnd != true) {
+      postRetrieved = false;
+      print("POST RETRIEVED IS: " + postRetrieved.toString());
+      if (firstPostTime == null) {
+        print("IS NULL");
+        var url = "http://postea-server.herokuapp.com/getTrendingPosts";
+        resp = await http.get(url);
+
+        postRetrieved = true;
+        print("POST RETRIEVED IS: " + postRetrieved.toString());
+
+        posts = jsonDecode(resp.body);
+        print(posts['error']);
+        if (posts['result'].length == 0 || posts['error'] == 1) {
+          print("Reached end");
+          isEnd = true;
+        }
+        print("OFFSET IS: " + offset.toString());
+        if (isEnd == false) {
+          firstPostTime = posts['result'][0]['creation_date'].toString();
+          var dateString = DateTime.parse(firstPostTime).toString();
+          print(dateString.substring(0, dateString.length - 5));
+          firstPostTime = dateString.substring(0, dateString.length - 5);
+
+          await processPosts();
+        }
+      } else {
+        print("IS NOT NULL");
+        var url =
+            "http://postea-server.herokuapp.com/getTrendingPosts&post_time='" +
+                firstPostTime +
+                "'";
+        resp = await http.get(url);
+
+        postRetrieved = true;
+        print("POST RETRIEVED IS: " + postRetrieved.toString());
+
+        posts = jsonDecode(resp.body);
+        print(posts['error']);
+        if (posts['result'].length == 0 || posts['error'] == 1) {
+          print("Reached end");
+          isEnd = true;
+        }
+        print("OFFSET IS: " + offset.toString());
+        if (isEnd == false) {
+          processPosts();
+        }
+      }
+    }
+
+    return resp;
+  }
+
+  processPosts() async {
+    for (int i = 0; i < posts['result'].length; i++) {
+      http.Response resp = await http.get(
+          "http://postea-server.herokuapp.com/profile/" +
+              posts['result'][i]['profile_id'].toString());
+      Map<String, dynamic> profileJson = jsonDecode(resp.body);
+      // print(profileJson['message']['name']);
+      Post newPost = Post(
+          posts['result'][i]['post_id'].toString(),
+          posts['result'][i]['profile_id'].toString(),
+          posts['result'][i]['post_description'].toString(),
+          posts['result'][i]['topic_id'].toString(),
+          posts['result'][i]['post_img'].toString(),
+          posts['timeDiff'][i].toString(),
+          posts['result'][i]['post_likes'].toString(),
+          posts['result'][i]['post_dislikes'].toString(),
+          posts['result'][i]['post_comments'].toString(),
+          posts['result'][i]['post_title'].toString(),
+          profileJson['message']['name']
+          // "Darshil Kaneria"
+          );
+      print(posts['result'][i]['post_id']);
+      postList.add(newPost);
+    }
+    print(postList.length);
+  }
+
+  setOffset(offset) {
+    this.offset = offset;
+  }
+
+  clearTimeline() {
+    postList.clear();
+    this.offset = 0;
+    this.isEnd = false;
+    firstPostTime = null;
+    print("DONE");
+  }
+}
