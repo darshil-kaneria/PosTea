2785d12402981fd348eee8dbb8df3daa4a762e26
shah444 <shah444@purdue.edu>
2020-11-18 21:22:22 +0530

    Added basic set up for change password functionality

--- front-end/postea_frontend/lib/pages/securitySettings.dart
+++ front-end/postea_frontend/lib/pages/securitySettings.dart
@@ -6,6 +6,10 @@ class SecuritySettings extends StatefulWidget {
 }
 
 class _SecuritySettingsState extends State<SecuritySettings> {
+  var passwordController = new TextEditingController();
+  var newPasswordController = new TextEditingController();
+  var confirmNewPasswordController = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     var screenWidth = MediaQuery.of(context).size.width;
@@ -35,7 +39,112 @@ class _SecuritySettingsState extends State<SecuritySettings> {
                       padding: const EdgeInsets.only(right: 10),
                       child: Icon(Icons.lock),
                     ),
-                    Text("Change Password", style: TextStyle(fontSize: 20)),
+                    InkWell(
+                        onTap: () {
+                          showDialog(
+                            context: context,
+                            barrierDismissible: false,
+                            builder: (context) {
+                              return StatefulBuilder(
+                                builder: (context, setState) {
+                                  return WillPopScope(
+                                    onWillPop: () async {
+                                      passwordController.clear();
+                                      newPasswordController.clear();
+                                      return true;
+                                    },
+                                    child: Dialog(
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.all(
+                                          Radius.circular(30),
+                                        ),
+                                      ),
+                                      child: Container(
+                                        width: screenWidth / 3,
+                                        height: screenHeight / 2.75,
+                                        child: Column(
+                                          children: [
+                                            Padding(
+                                              padding: const EdgeInsets.only(
+                                                  left: 15, top: 10, right: 25),
+                                              child: TextField(
+                                                controller: passwordController,
+                                                textAlign: TextAlign.left,
+                                                decoration: InputDecoration(
+                                                    enabledBorder:
+                                                        UnderlineInputBorder(),
+                                                    border: InputBorder.none,
+                                                    hintText:
+                                                        "Enter current password"),
+                                              ),
+                                            ),
+                                            Padding(
+                                              padding: const EdgeInsets.only(
+                                                  left: 15,
+                                                  top: 10,
+                                                  right: 25,
+                                                  bottom: 10),
+                                              child: TextField(
+                                                controller:
+                                                    newPasswordController,
+                                                textAlign: TextAlign.left,
+                                                decoration: InputDecoration(
+                                                    enabledBorder:
+                                                        UnderlineInputBorder(),
+                                                    border: InputBorder.none,
+                                                    hintText:
+                                                        "Enter new password"),
+                                              ),
+                                            ),
+                                            Padding(
+                                              padding: const EdgeInsets.only(
+                                                  left: 15,
+                                                  top: 10,
+                                                  right: 25,
+                                                  bottom: 10),
+                                              child: TextField(
+                                                controller:
+                                                    confirmNewPasswordController,
+                                                textAlign: TextAlign.left,
+                                                decoration: InputDecoration(
+                                                    enabledBorder:
+                                                        UnderlineInputBorder(),
+                                                    border: InputBorder.none,
+                                                    hintText:
+                                                        "Confirm new password"),
+                                              ),
+                                            ),
+                                            ButtonTheme(
+                                              shape: RoundedRectangleBorder(
+                                                  borderRadius:
+                                                      BorderRadius.circular(
+                                                          20)),
+                                              child: RaisedButton(
+                                                  child:
+                                                      Text("Change Password"),
+                                                  onPressed: () {
+                                                    String currentPassword =
+                                                        passwordController.text;
+                                                    String newPassword =
+                                                        newPasswordController
+                                                            .text;
+                                                    String confirmNewPassword =
+                                                        confirmNewPasswordController
+                                                            .text;
+                                                  }),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  );
+                                },
+                              );
+                            },
+                          );
+                        },
+                        child: Text("Change Password",
+                            style: TextStyle(fontSize: 20))),
                     Spacer(),
                     Icon(Icons.arrow_forward_ios)
                   ],
